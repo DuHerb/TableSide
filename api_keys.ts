152dc4b0e825f53fa301b52a9956b5d15074dbdf
Student <example@example.com>
2019-07-25 16:06:29 -0700

    add firebase creds to repo

---
+++ api_keys.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyC787Z-bNh2MxEXTGduD9SB4qZZyE3tsmI",
+    authDomain: "tableside-dustin.firebaseapp.com",
+    databaseURL: "https://tableside-dustin.firebaseio.com",
+    projectId: "tableside-dustin",
+    storageBucket: "tableside-dustin.appspot.com",
+    messagingSenderId: "1097189906818",
+    appId: "1:1097189906818:web:c5eb113b85320ac7"
+  };
